228baf389e53ad45fe922f81b1b69fc2df562e8c
TomasNagy <tomi.nagy@outlook.com>
2023-05-07 10:13:17 +0200

    Add comments

--- creditCard.c
+++ creditCard.c
@@ -1,5 +1,5 @@
+#include <cs50.h>
 #include <stdio.h>
-#include <cs50.c>
 
 long long get_cardNumber(void);
 int getDigit(long long cardNumber, int digit);
@@ -73,6 +73,10 @@ long long get_cardNumber(void)
 // Get the choosen digit from the card number
 int getDigit(long long cardNumber, int digit)
 {
+	//To get the digit I take the modulo of the number then divide it
+	//Example: Number = 123 and i want to get the 2nd digit
+	//The calculation would be the next: (123 % (10 on power of 2)) / (10 on power of 1) = (123 % 100) / 10 = 23 / 10 = 2
+	// any number on power of 1 is 1 and on power of 0 is 1, this way we can calculate the first digit (ones place) too
 	return (cardNumber % powerOf(10, digit)) / powerOf(10, digit-1);
 }
 
@@ -95,13 +99,14 @@ long long powerOf(int num, int power)
 // Get card type 
 int get_cardType(long long cardNumber)
 {
+	// Get the last 4 digits of the card number
 	int digit13 = getDigit(cardNumber, 13);
 	int digit14 = getDigit(cardNumber, 14);
 	int digit15 = getDigit(cardNumber, 15);
 	int digit16 = getDigit(cardNumber, 16);
 
 	// MASTERCARD = 16 digits; start with 51,52,53,54 or 55
-	if (digit16 == 5 && (digit15 == 1 || digit15 == 2 || digit15 == 3 || digit15 == 4 || digit15 == 5))
+	if (digit16 == 5 && digit15 >= 1 && digit15 <= 5)
 	{
 		return 1;
 	}
@@ -124,8 +129,41 @@ int get_cardType(long long cardNumber)
 		return 4;
 	}
 
+	// Invalid card
 	return 0;
 }
+
+/*Most cards use an algorithm invented by Hans Peter Luhn of IBM. 
+According to Luhn’s algorithm, you can determine if a credit card number is (syntactically) valid as follows:
+
+1. Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
+2. Add the sum to the sum of the digits that weren’t multiplied by 2 
+3. If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
+
+That’s kind of confusing, so let’s try an example with Visa: 4003600000000014.
+
+1. For the sake of discussion, let’s first mark ( put in () ) every other digit, 
+starting with the number’s second-to-last digit:
+
+	(4) 0 (0) 3 (6) 0 (0) 0 (0) 0 (0) 0 (0) 0 (1) 4
+
+	Okay, let’s multiply each of the marked (in () ) digits by 2:
+
+	4x2 + 0x2 + 6x2 + 0x2 + 0x2 + 0x2 + 0x2 + 1x2
+
+	That gives us:
+
+	8 + 0 + 12 + 0 + 0 + 0 + 0 + 2
+
+	Now let’s add those products’ digits (i.e., not the products themselves) together:
+
+	8 + 0 + 1 + 2 + 0 + 0 + 0 + 0 + 2 = 13
+
+2. Now let’s add that sum (13) to the sum of the digits that weren’t multiplied by 2 (starting from the end):
+
+	13 + 4 + 0 + 0 + 0 + 0 + 0 + 3 + 0 = 20
+
+3. The last digit in that sum (20) is a 0, so the card is legit! */
 
 bool checkCardNumValidity(long long cardNumber, int cardType)
 {
